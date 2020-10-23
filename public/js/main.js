8d8f26983917d60b585618d65e6d0df90705e9db
stormymayday <stormymayday7@gmail.com>
2020-10-23 13:53:57 -0600

    Implemented the Navbar.

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,9 @@
-/*
-	Photon by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
+// Navbar Scripr
+const toggleButton = document.getElementsByClassName('toggle-button')[0];
+const navbarLinks = document.getElementsByClassName('navbar-links')[0];
+toggleButton.addEventListener('click', () => {
+	navbarLinks.classList.toggle('active');
+});
 
 // async function getLotImage(imageID) {
 // 	const imageid = imageID;
